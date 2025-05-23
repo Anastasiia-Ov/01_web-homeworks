43c8bd71f2a1d62b84e18aa08d89d7ad41680436
Anastasiia Ovechkina <an239669@yandex.com>
2025-05-23 23:59:08 +0700

    startServer implemented

---
+++ src/main/java/ru/netology/Server.java
@@ -0,0 +1,106 @@
+package ru.netology;
+
+import java.io.BufferedOutputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Server {
+    private static final Integer PORT = 8379;
+    private final List<String> validPaths = new ArrayList<>();
+
+    public Server(List<String> validPaths) {
+        this.validPaths.addAll(validPaths);
+    }
+
+    // метод запуска
+    public void startServer() {
+        try (final ServerSocket serverSocket = new ServerSocket(PORT)) {
+            while (true) {
+                try (Socket socket = serverSocket.accept()) {
+                    processConnection(socket);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // метод обработки конкретного подключения
+    public void processConnection(Socket socket) {
+        try (BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+             BufferedOutputStream out = new BufferedOutputStream(socket.getOutputStream())) {
+
+            // must be in form GET /path HTTP/1.1
+            final String requestLine = in.readLine();
+            final String[] parts = requestLine.split(" ");
+
+            if (parts.length != 3) return;
+
+            final String path = parts[1];
+            if (isError404(out, path)) return;
+
+            final Path filePath = Path.of(".", "public", path);
+            final String mimeType = Files.probeContentType(filePath);
+            if (path.equals("/classic.html")) return;
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public boolean isError404(BufferedOutputStream out, String path) throws IOException{
+        if (!validPaths.contains(path)) {
+            out.write((
+                    "HTTP/1.1 404 Not Found\r\n" +
+                            "Content-Length: 0\r\n" +
+                            "Connection: close\r\n" +
+                            "\r\n"
+            ).getBytes());
+            out.flush();
+            return true;
+        }
+        return false;
+    }
+
+    // special case for classic
+//    public void specialForClassic() {
+//        if (path.equals("/classic.html")) {
+//            final var template = Files.readString(filePath);
+//            final var content = template.replace(
+//                    "{time}",
+//                    LocalDateTime.now().toString()
+//            ).getBytes();
+//            out.write((
+//                    "HTTP/1.1 200 OK\r\n" +
+//                            "Content-Type: " + mimeType + "\r\n" +
+//                            "Content-Length: " + content.length + "\r\n" +
+//                            "Connection: close\r\n" +
+//                            "\r\n"
+//            ).getBytes());
+//            out.write(content);
+//            out.flush();
+//            continue;
+//        }
+//
+//        final var length = Files.size(filePath);
+//        out.write((
+//                "HTTP/1.1 200 OK\r\n" +
+//                        "Content-Type: " + mimeType + "\r\n" +
+//                        "Content-Length: " + length + "\r\n" +
+//                        "Connection: close\r\n" +
+//                        "\r\n"
+//        ).getBytes());
+//        Files.copy(filePath, out);
+//        out.flush();
+//
+//    }
+}
